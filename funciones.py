438fb04e50479b730f9ab3d02e5dcc1da37c6e75
mateoraviolo <97976771+mraviolo@users.noreply.github.com>
2022-08-11 22:11:15 -0300

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,8 +1,7 @@
+# importamos text para imprimir los datos de forma mas ordenada
 from cgitb import text
 
-from entidades import Circuito
-
-
+# diccionario con todos los equipos y sus pilotos
 equipos_pilotos = { 
                 'FERRARI':'CARLOS SAINZ''CHARLES LECRECR', 
                 'RED BULL':'MAX VERSTAPPEN''SERGIO PEREZ', 
@@ -16,6 +15,7 @@ equipos_pilotos = {
                 'ASTON MARTIN': 'SEBASTIAN VETTEL''LANCE STROLL'
                 }
 
+# lista con todos los pilotos 
 pilotos = [
     'CARLOS SAINZ',
     'CHARLES LECRECR',
@@ -39,6 +39,7 @@ pilotos = [
     'LANCE STROLL'
     ]
 
+# lista con todos los equipos 
 equipos = [
     'FERRARI',
     'RED BULL',
@@ -52,6 +53,7 @@ equipos = [
     'ASTON MARTIN'
     ]
 
+# lista con todos los circuitos
 circuitos = [
     'BAHRAIN'
     'JEDDAH',
@@ -77,7 +79,10 @@ circuitos = [
     'YAS MARINA'
     ]
 
+# funcion menu
 def menu ():
+    # mensaje informativo para el user
+    # todas las opciones que tiene para hacer
     print(
         '-------------------------------------------------------------------------\n'
         '-Hola, esta es una app que te va mostrar toda la informacion sobre la F1-\n'
@@ -90,74 +95,111 @@ def menu ():
         '6) salir'
     )
 
+# funcion pil_equ (equipos y pilotos)
 def pil_equ (equipos_pilotos):
+    # imprimimos todos los equipos con sus pilotos de manera prolija usando text
     texto = ''
     for piloto_equipo in equipos_pilotos:
         texto += str(piloto_equipo) + '\n'
     return texto
 
-
+# funcion cir (circuitos)
 def cir(circuitos):
+    # imprimimos todos los circuitos de manera prolija usando text
     texto = ''
     for circuito in circuitos:
         texto += str(circuito) + '\n'
     return texto
 
+# funcion equ (equipos)
 def equ (equipos):
+    # imprimimos todos los equipos de manera prolija usando text
     texto = ''
     for equipo in equipos:
         texto += str(equipo) + '\n'
     return texto
 
+# funcion pil (pilotos)
 def pil (pilotos):
+    # imprimimos todos los pilotos de manera prolija usando text
     texto = ''
     for piloto in pilotos:
         texto += str(piloto) + '\n'
     return texto
 
+# funcion cal (calendario)
 def cal ():
     pass
 
+# funcion mi_equipo
 def mi_equipo ():
-
+        # pedimos al user que equipo quiere usar
         equipo = input('introduce tu equipo: ')
+        # convertimos equipo a mayusculas para que no salga error al buscarlo en la lista
         equipo = equipo.upper()
+        # bucle infinito hasta que se introduzca un equipo valido
         while True:
+            # analizamos si existe ese equipo
             if equipo in equipos:
+                # mensaje informativo indicando que se selecciono correctamente
                 print(f'perfecto, has seleccionado la scuderia{equipo}')
+                # salimos del bucle
                 break
             else:
+                # mensaje informativo indicando que no existe ese equipo
                 print(f'incorrecto, la scuderia introducida {equipo} no exite')
+                # mostramos los equipos disponibles
                 print(equ(equipos))
+                # pedimos el equipo al user de vuelta
                 equipo = input('introduce tu equipo: ')
+                # convertimos a mayusculas el equipo
                 equipo = equipo.upper()
 
+        # pedimos al user que piloto principal quiere usar
         piloto1 = input('introduce el piloto principal: ')
+        # convertimos piloto1 a mayusculas para que no salga error al buscarlo en la lista
         piloto1 = piloto1.upper()
-
+        # bucle infinito hasta que se introduzca un piloto valido
         while True:
+            # analizamos si existe ese piloto
             if piloto1 in pilotos:
+                # mensaje informativo indicando que se selecciono correctamente
                 print(f'perfecto, has seleccionado a {piloto1} como piloto principal')
+                # salimos del bucle
                 break
             else:
+                # mensaje informativo indicando que no exite ese piloto
                 print(f'incorrecto, el piloto {piloto1} no exite')
+                # mostramos los pilotos disponibles
                 print(pil(pilotos))
+                # pedimos el piloto al user de vuelta
                 piloto1 = input('introduce el piloto principal: ')
+                # convertimos a mayusculas el piloto1
                 piloto1 = piloto1.upper()
-        
-        piloto2 = input('introduce el piloto secundario: ')
-        piloto2 = piloto2.upper()
 
+        # pedimos al user que piloto secundario quiere usar
+        piloto2 = input('introduce el piloto secundario: ')
+        # convertimos piloto2 a mayusculas para que no salga error al buscarlo en la lista
+        piloto2 = piloto2.upper()
+        # bucle infinito hasta que se introduzca un piloto valido
         while True:
+            # analizamos si existe ese piloto
             if piloto2 in pilotos:
+                # mensaje informativo indicando que se selecciono correctamente
                 print(f'perfecto, has seleccionado a {piloto2} como piloto principal')
+                # salimos del bucle
                 break
             else:
+                # mensaje informativo indicando que no exite ese piloto
                 print(f'incorrecto, el piloto {piloto2} no exite')
+                # mostramos los pilotos disponibles
                 print(pil(pilotos))
+                # pedimos el piloto al user de vuelta
                 piloto2 = input('introduce el piloto principal: ')
+                # convertimos a mayusculas el piloto2
                 piloto2 = piloto2.upper()
 
+        # imprimimos toda la informacion
         print(
             '-------------------------\n'
             f'equipo: {equipo}\n'
--- main.py
+++ main.py
@@ -1,56 +1,96 @@
+# importamos las funciones
 from funciones import *
-from tkinter import *   
 
+# bucle infinito para ejecutar el programa
 while True:
 
+    # llamamos a la funcion menu
     menu()
 
+    # pedimos al user que quiere hacer
     operacion = int(input('>>> '))
 
+    # analizamos la operacion
+    # opcion 1
     if operacion == 1:
+        # imprimimos todos los circuitos
+        # llamamos a la funcion cir
         print(cir(circuitos))
+        # opciones de circuitos
         print(
             '1) ver imagen del circuito\n'
-            '2) ver infomacion del circuito'
+            '2) ver infomacion del circuito\n'
+            '3) volver'
         )
+        # pedimos al user si quiere hacer alguna de esas operaciones
         operacion = int(input('>>> '))
+        # opcion 1
         if operacion == 1:
             pass
+        # opcion 2
         elif operacion == 2:
             pass
-
+    
+    # opcion 2
     elif operacion == 2:
+        # imprimimos todos los equipos 
+        # llamamos a la funcion equ
         print(equ(equipos))
+        # opciones de equipos
         print(
             '1) ver historia de la escuderia\n'
-            '2) ver los pilotos que pertenecen a la escuderia'
+            '2) ver los pilotos que pertenecen a la escuderia\n'
+            '3) volver'
         )
+        # pedimos al user si quiere hacer alguna de esas operaciones
         operacion = int(input('>>> '))
+        # opcion 1
         if operacion == 1:
             pass
+        # opcion 2
         elif operacion == 2:
+            # imprimimos los equipos con sus respectivos pilotos
+            # llamamos la funcion pil_equ
             print(pil_equ(equipos_pilotos))
 
+    # opcion 3
     elif operacion == 3:
+        # imprimimos todos los pilotos
+        # llamamos a la funcion pil
         print(pil(pilotos))
+        # opciones de pilotos
         print(
             '1) ver informacion personal del piloto\n'
-            '2) ver escuderias a las que pertenecen'
+            '2) ver escuderias a las que pertenecen\n'
+            '3) volver'
         )
+        # pedimos al user si quiere hacer alguna de esas operaciones
         operacion = int(input('>>> '))
+        # opcion 1
         if operacion == 1:
             pass
+        # opcion 2
         elif operacion == 2:
+            # imprimimos los pilotos con sus respectivos equipos
+            # llamamos a la funcion pil_equ
             print(pil_equ(equipos_pilotos))
 
+    # opcion 4
     elif operacion == 4:
         pass
     
+    # opcion 5
     elif operacion == 5:
+        # imprimimos su equipo armado por el
+        # llamamos a la funcion mi_equipo
         print(mi_equipo())
-    
+
+    # opcion 6
     elif operacion == 6:
+        # cerramos el programa
         break
     
+    # ninguna de las opciones
     else:
+        # mensaje de opcion no disponible para el user
         print('valor incorrecto')
